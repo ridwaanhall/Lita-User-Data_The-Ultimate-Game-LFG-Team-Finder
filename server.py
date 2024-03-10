96704352438f8ab7a0f27c8ae775435ec4565704
ridwaanhall <erbyl14@gmail.com>
2024-03-10 15:57:48 +0700

    Player detail🧿

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from FetchURL.fetch_data import DataFetcher
 from Controller.player_inskill import PlayerInSkill
+from Controller.player_detail import PlayerDetail
 
 if __name__ == "__main__":
-    player_inskill = PlayerInSkill()
-    data_fetcher = DataFetcher(player_inskill.player_inskill_func())
+    player_inskill = PlayerInSkill().player_inskill_func()
+    player_detail  = PlayerDetail(18682745).player_detail_func()
+
+    data_fetcher = DataFetcher(player_detail)
     data, status_code = data_fetcher.fetch_data()
 
     print(data)
