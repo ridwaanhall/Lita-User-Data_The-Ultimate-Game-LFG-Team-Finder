f3afee91d4c2759686a24c76f0554ffde62775d0
ridwaanhall <erbyl14@gmail.com>
2024-03-11 08:45:23 +0700

    Add comment

--- server.py
+++ server.py
@@ -19,10 +19,10 @@ moment_user_list = MomentUserList(id).moment_user_list_func() # moment user list
 
 if __name__ == "__main__":
     gender = 2
-    skillId = 1
+    skillId = 40
     rows = 50
 
-    for page in range(1, 447):
+    for page in range(1, 500):
         json_data = PlayerInSkill(gender, skillId, page, rows).player_inskill_func() # list player in skill
 
         data_fetcher = DataFetcher(json_data)
@@ -31,9 +31,9 @@ if __name__ == "__main__":
         handle_response = HandleResponsePiS()
         response = handle_response.handle_data_func(data)
         
-        response = json.dumps(response, indent=4)
+        response = json.dumps(response, indent=4) # convert json to string
 
-        response_json = json.loads(response)
+        response_json = json.loads(response) # convert response to json
 
         if response_json['status'] != '0':
             print(f"Data not available for page {page}, stopping...")
